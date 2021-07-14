109c5670791ae0a357a144bff6a95ad4f81c0a73
ruadoian <ruadoian@gmail.com>
2021-07-14 23:38:31 +0900

    Styled Components

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react"
+import  Jumbotron from "../components/jumbotron/index"
+
+export default function Home(){
+    return(
+       <Jumbotron.Container>
+           <Jumbotron.Title>Hello there from Home!</Jumbotron.Title>
+       </Jumbotron.Container>
+    )
+}
